1fa69a6bead2bbd2c096469906234900af36362c
constantinrazvan <76180811+constantinrazvan@users.noreply.github.com>
2024-09-13 01:47:11 +0300

    Created the becomeVolutneerService

--- Services/BecomeVolunteerService.cs
+++ Services/BecomeVolunteerService.cs
@@ -1,27 +1,74 @@
 ﻿using AsmvBackend.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using ServerAsmv.Data;
 using ServerAsmv.Interfaces;
 
 namespace ServerAsmv.Services {
     public class BecomeVolunteerService : IBecomeVolunteer
     {
-        public void AddBecomeVolunteer(BecomeVolunteer newBecomeVolunteer)
+        private readonly AppData _context;
+
+        public BecomeVolunteerService(AppData context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public void DeleteBecomeVolunteer(long Id)
+        public void AddBecomeVolunteer(BecomeVolunteer newBecomeVolunteer)
         {
-            throw new NotImplementedException();
+            if(newBecomeVolunteer == null) {
+                throw new ArgumentNullException("Request cannot be null!");
+            }
+
+            BecomeVolunteer newVolunteer = new BecomeVolunteer(
+                fullname: newBecomeVolunteer.Fullname,
+                email: newBecomeVolunteer.Email,
+                phone: newBecomeVolunteer.Phone,
+                faculty: newBecomeVolunteer.Faculty,
+                reason: newBecomeVolunteer.Reason,
+                newRequest: true
+            );
+
+            _context.BecomeVolunteers.Add(newVolunteer);
+            Console.WriteLine("newBecomeVolunteer has been added");
+            Console.WriteLine($"{newBecomeVolunteer}");
+        }
+
+        public async void DeleteBecomeVolunteer(long Id)
+        {
+            BecomeVolunteer? found = await _context.BecomeVolunteers.FindAsync(Id);
+
+            if(found == null) {
+                throw new KeyNotFoundException($"User with ID {Id} has not be found!");
+            }
+
+            _context.BecomeVolunteers.Remove(found);
+            _context.SaveChanges();
         }
 
         public BecomeVolunteer GetBecomeVolunteer(long Id)
         {
-            throw new NotImplementedException();
+            BecomeVolunteer found = _context.BecomeVolunteers.FindAsync(Id).Result!;
+
+            if(found == null) {
+                throw new KeyNotFoundException("BecomeVolunteer not found");
+            }
+
+            return found;
         }
 
         public List<BecomeVolunteer> GetBecomeVolunteers()
         {
-            throw new NotImplementedException();
+            List<BecomeVolunteer> becomeVolunteers = new List<BecomeVolunteer>();
+
+            for(int i = 0; i < _context.BecomeVolunteers.Count(); i++) {
+                becomeVolunteers.Add(_context.BecomeVolunteers.ElementAt(i));
+            }
+
+            if(becomeVolunteers.Count == 0) {
+                throw new KeyNotFoundException("No BecomeVolunteers found");
+            }
+
+            return becomeVolunteers;
         }
 
         public bool UpdateBecomeVolunteer(BecomeVolunteer newBecomeVolunteer)
