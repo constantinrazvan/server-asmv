9fefc3fb9466ebe9a0e219743b1e1dda30466b70
constantinrazvan <76180811+constantinrazvan@users.noreply.github.com>
2024-09-13 16:53:20 +0300

    created the auth controller

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,11 +1,57 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ServerAsmv.DTOs;
+using ServerAsmv.Services;
 
 namespace ServerAsmv.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
     public class AuthController : ControllerBase
     {
+        private readonly AuthService _service;
+
+        public AuthController(AuthService service)
+        {
+            this._service = service;
+        }
+
+        [HttpPost("/login")]
+        public IActionResult Login([FromBody] LoginDTO user)
+        {
+            if (user == null)
+            {
+                return BadRequest("Invalid request: user is null");
+            }
+
+            string token = _service.login(user);
+
+            if (string.IsNullOrEmpty(token))
+            {
+                return BadRequest("Something happened!");
+            }
+
+            return Ok(token);
+        }
+
+        [HttpPost("/register")]
+        public IActionResult Register([FromBody] RegisterDTO register)
+        {
+            if (register == null)
+            {
+                return BadRequest("Invalid request! User is null!");
+            }
+
+            bool result = _service.register(register);
+
+            if (result)
+            {
+                return Ok("User created successfully!");
+            }
+            else
+            {
+                return BadRequest("User already exists!");
+            }
+        }
     }
 }
